df44926f24eb5f3330e5a302c39166fe3e48160f
Amel <amel.neffati@epitech.eu>
2018-10-05 14:15:25 +0200

    views

---
+++ ccsbn/web/assets/js/add_class.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#fos_user_profile_form_username').addClass('input');
+    $('#fos_user_profile_form_email').addClass('input');
+    $('#fos_user_profile_form_current_password').addClass('input');
+});
